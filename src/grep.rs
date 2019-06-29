0c84fc057df14968dd7ee04c35c5761ac66a104e
Samuel Molinari <samuel@molinari.me>
2019-06-29 12:47:58 +0100

    Highlight matches by lines

--- src/grep.rs
+++ src/grep.rs
@@ -1,42 +1,95 @@
 use crate::cmd::Config;
 use std::fs::File;
-use std::io;
+use std::io::{self, BufReader};
 use std::io::prelude::*;
 
-pub fn find(config: Config) -> io::Result<()> {
-    let open_tag = "\u{001b}[31m";
-    let close_tag = "\u{001b}[0m";
-    let extra_chars_size = open_tag.len() + close_tag.len();
+const OPEN_TAG: &str = "\u{001b}[1;93m";
+const CLOSE_TAG: &str = "\u{001b}[0m";
 
-    let size = config.query.len();
-    let content = get_content(&config.filepath)?;
-    let mut content_matched = content.clone();
-    let matches = search(&content, &config.query);
+struct QueryMatch<'a> {
+    column: usize,
+    value: &'a str
+}
 
-    for (i, a_match) in matches.iter().enumerate() {
-        let extra = extra_chars_size * i;
-        let range = (
-            a_match.0 + extra,
-            a_match.0 + extra + size + open_tag.len()
-        );
-        content_matched.insert_str(range.0, open_tag);
-        content_matched.insert_str(range.1, close_tag);
+impl<'a> QueryMatch<'a> {
+    pub fn from(match_index: &(usize, &'a str)) -> QueryMatch<'a> {
+        QueryMatch {
+            column: match_index.0,
+            value: match_index.1
+        }
     }
 
-    println!("{}", content_matched);
+    pub fn len(&self) -> usize {
+        self.value.len()
+    }
+}
+
+struct Highlight {
+    row: usize,
+    column: usize,
+    value: String
+}
+
+impl Highlight {
+    fn println(&self) {
+        println!(
+            "\u{001b}[1m{}:{}\u{001b}[0m\t{}",
+            self.row,
+            self.column,
+            self.value
+        )
+    }
+
+    fn from(query_match: QueryMatch, line: Line) -> Highlight {
+        let mut value = line.value.clone();
+        let start = query_match.column;
+        let end = start + query_match.len() + OPEN_TAG.len();
+
+        Highlight::tag(&mut value, start, end);
+
+        Highlight {
+            row: line.position,
+            column: start,
+            value
+        }
+    }
+
+    fn tag(value: &mut String, start: usize, end: usize) {
+        value.insert_str(start, OPEN_TAG);
+        value.insert_str(end, CLOSE_TAG);
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Line<'a> {
+    position: usize,
+    value: &'a String
+}
+
+pub fn find(config: Config) -> io::Result<()> {
+    let file = File::open(config.filepath)?;
+
+    for (index, value) in BufReader::new(file).lines().enumerate() {
+        search_line(
+            Line { value: &value?, position: index + 1 },
+            &config.query
+        );
+    }
 
     Ok(())
 }
 
-fn search<'a>(content: &'a String, query: &str) -> Vec<(usize, &'a str)> {
-    content.match_indices(query).collect()
+fn search_line(line: Line, query: &str) {
+    let matches = search(line.value, query);
+
+    for a_match in matches.iter() {
+        Highlight::from(
+            QueryMatch::from(&a_match),
+            line
+        ).println()
+    }
 }
 
-fn get_content(filepath: &str) -> io::Result<String> {
-    let mut content: String = String::new();
-
-    File::open(filepath)?
-        .read_to_string(&mut content)?;
-
-    Ok(content)
+fn search<'a>(content: &'a String, query: &str) -> Vec<(usize, &'a str)> {
+    content.match_indices(query).collect()
 }
